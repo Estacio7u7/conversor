c5ae4c1dcbb3e31e58b44df6d5e93d9931f9a071
David Estacio <festacio00@gmail.com>
2023-09-05 21:25:30 -0500

    Conversión de moneda y presion funcionando, hace falta trabajar en la temperatura.

--- menuPrincipal.java
+++ menuPrincipal.java
@@ -1,9 +1,20 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 
 import javax.swing.JOptionPane;
 
 public class menuPrincipal {
     public static void main(String[] args) {
+        // para convertir necesitamos:
+        double quantity; // la cantidad inicial
+        double convertionFactor; // el factor de conversión
+        double result; //el resultado de la conversión
         
         // Menú principal con las opciones de conversión
         String[] convOptions = {
@@ -24,21 +35,25 @@ public class menuPrincipal {
         );
 
         String magnitude = "";
+        String folder = "";
         switch (selectedValue.toString()) {
             case "Moneda":
                 magnitude = "la cantidad de dinero";
+                // Ruta de la carpeta que contiene los archivos .properties
+                folder = "./Moneda";
             break;
             case "Temperatura":
                 magnitude = "los grados de entrada";
+                folder = "./Temperatura";
             break;
             case "Presión":
                 magnitude = "la magnitud de presión";
+                folder = "./Presion";
             break;
         }
         
         // Solicitar repetidamente el dato inicial
         String initialQtty = new String();
-        double quantity;
 
         while(true){ 
             // Solicitar la cantidad
@@ -59,8 +74,31 @@ public class menuPrincipal {
                 );
             }
         }
-
+        
         // Menú secundario con las opciones de conversión disponibles
+
+        // Crear una lista para almacenar los nombres de archivos .properties
+        List<String> posibleConvertions = new ArrayList<>();
+
+        // Crear un objeto File para representar la carpeta
+        File file = new File(folder);
+
+        // Verificar si la carpeta existe
+        if (file.exists() && file.isDirectory()) {
+            // Listar los archivos en el directorio
+            File[] archivos = file.listFiles();
+
+            // Iterar a través de los archivos y agregar los nombres de .properties a la lista
+            for (File archivo : archivos) {
+                if (archivo.isFile() && archivo.getName().endsWith(".properties")) {
+                    String[] name = archivo.getName().split("\\.");
+                    posibleConvertions.add(name[0]);
+                }
+            }
+        } else {
+            posibleConvertions.add("No hay diccionarios disponibles");
+        }
+        /*
         ArrayList<String> qttyUnits = new ArrayList<>();
 
         switch (selectedValue.toString()) {
@@ -87,8 +125,10 @@ public class menuPrincipal {
             break;
         };
 
+        */
+
         // Obtener la selección de unidad inicial del usuario
-        String[] array = qttyUnits.toArray(new String[qttyUnits.size()]);
+        //String[] array = qttyUnits.toArray(new String[qttyUnits.size()]);
 
         Object initialUnit = JOptionPane.showInputDialog(
             null,
@@ -96,14 +136,30 @@ public class menuPrincipal {
              String.format("Conversor de Moneda - Convirtiendo %s", selectedValue.toString()),
              JOptionPane.INFORMATION_MESSAGE,
              null,
-             array,
-             array[0]
+             posibleConvertions.toArray(),
+             posibleConvertions.toArray()[0]
         );
 
-        // remover la selección de la lista de opciones
-        qttyUnits.remove(initialUnit);
-        array = qttyUnits.toArray(new String[qttyUnits.size()]);
+        // cargar el diccionario con las conversiones requeridas
+        Properties availableConv = new Properties();
+        try(InputStream inputDictionary = new FileInputStream("./dictionaries/"+initialUnit.toString()+".properties")){
+            availableConv.load(inputDictionary);
+            //availableConv.forEach((key, value) -> System.out.println(key + " = " + value));
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(
+                null,
+                "Existe un problema al tratar de cargar el diccionario: " + e.getMessage(),
+                "Error de Diccionarios",
+                JOptionPane.ERROR_MESSAGE);
+        }
 
+        // remover la selección de la lista de opciones
+        //qttyUnits.remove(initialUnit);
+
+        // mostrar la lista de conversiones disponibles
+        Set<Object> convDisponibles = availableConv.keySet();
+        String[] keys = convDisponibles.toArray(new String[convDisponibles.size()]);
+                
         // Obtener la selección de unidad final del usuario
         Object finalUnit = JOptionPane.showInputDialog(
             null,
@@ -111,10 +167,22 @@ public class menuPrincipal {
              String.format("Conversor de Moneda - Convirtiendo %s", selectedValue.toString()),
              JOptionPane.INFORMATION_MESSAGE,
              null,
-             array,
-             array[0]
+             keys,
+             keys[0]
         );
 
+        // el factor de conversión está en el diccionario
+        System.out.println(availableConv.get(finalUnit.toString()));
+        convertionFactor = Double.parseDouble(availableConv.get(finalUnit.toString()).toString());
+        
+        // realizar la conversión y mostrarla en pantalla
+        result = quantity * convertionFactor;
+
+        JOptionPane.showMessageDialog(
+                null,
+                "La cantidad resultante es: " + result,
+                "Resultado de la conversión",
+                JOptionPane.INFORMATION_MESSAGE);
         
     }
 }
